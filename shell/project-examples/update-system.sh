fc39231ff7486cdcff6676f3f9a37d8ac38116d1
Guilherme Moraes <guilherme.alan.moraes@gmail.com>
2024-12-21 15:18:46 -0300

    Create update-system.sh
    script to update linux apt and flatpak packages

---
+++ shell/project-examples/update-system.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+print_header() {
+    local message=$1
+    local length=$(printf "%s" "$message" | wc -m)
+    local border=$(printf '═%.0s' $(seq 1 $length))
+
+    echo "╔═$border═╗"
+    echo "║ $message ║"
+    echo "╚═$border═╝"
+}
+
+update_apt() {
+    print_header "Atualizando apt"
+    if sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y; then
+        echo "APT atualizado com sucesso."
+    else
+        echo "Erro ao atualizar o APT." >&2
+    fi
+}
+
+update_flatpak() {
+    print_header "Atualizando flatpak"
+    if flatpak update -y && flatpak remove --unused --delete-data; then
+        echo "Flatpak atualizado com sucesso."
+    else
+        echo "Erro ao atualizar o Flatpak." >&2
+    fi
+}
+
+update_apt
+update_flatpak
+
