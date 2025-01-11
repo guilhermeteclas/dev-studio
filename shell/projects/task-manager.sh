6edc2cb28cfa59fb4e7868c1f53e0d9a074d8453
Guilherme Moraes <guilherme.alan.moraes@gmail.com>
2025-01-10 22:47:12 -0300

    update scripts

--- shell/projects/task-manager.sh
+++ shell/projects/task-manager.sh
@@ -7,19 +7,20 @@ ARROW_ICON=$(echo -e "\u27A1")
 
 ## Functions ##
 list_tasks() {
-    if [ ! -f "$FILE" ]; then
-        touch "$FILE.txt"
-    elif [ -s "$FILE" ]; then
-        echo -e "---- To-Do List ----\n"
-        nl "$FILE"
-        echo -e "\n--------------------"
-    else
-        echo "No tasks found."
-    fi
+  if [ -s "$FILE" ]; then
+    echo -e "---- To-Do List ----\n"
+    nl "$FILE"
+    echo -e "\n--------------------"
+  else
+    echo "No tasks found."
+  fi
 }
 
-
 add_task() {
+  if [ ! -f "$FILE" ]; then
+    touch "$FILE.txt"
+  fi
+  
   echo "Enter the task description:"
   read task
   echo "$task" >>"$FILE"
@@ -30,21 +31,21 @@ remove_task() {
   echo $FILE
 
   if [ -s "$FILE" ]; then
-		list_tasks
+    list_tasks
 
-	  echo -e "\nEnter the line number of the task to remove:"
-  	read line_number
-		
-	  if [ "$line_number" -gt 0 ] && [ "$line_number" -le $(wc -l <"$FILE") ]; then
-  	  sed -i "${line_number}d" "$FILE"
-	    echo "Task removed."
-	  else
-  	  echo "Invalid line number."
-	  fi
-	else
-		echo ""
+    echo -e "\nEnter the line number of the task to remove:"
+    read line_number
+
+    if [ "$line_number" -gt 0 ] && [ "$line_number" -le $(wc -l <"$FILE") ]; then
+      sed -i "${line_number}d" "$FILE"
+      echo "Task removed."
+    else
+      echo "Invalid line number."
+    fi
+  else
+    echo ""
     echo "Your to-do list is empty."
-	fi
+  fi
 }
 
 list_txt_files() {
@@ -65,6 +66,9 @@ list_txt_files() {
   fi
 }
 
+choose_file(){
+  read -p "Enter the file name: " FILE
+}
 
 # Main menu
 while true; do
@@ -73,8 +77,16 @@ while true; do
     list_txt_files $1
     echo -n -e $ARROW_ICON " Choose an file:\n"
     read FILE
+    FILE="${FILE}.txt"
+
+    if [ ! -f "$FILE" ]; then
+      touch "$FILE"
+      #touch "${FILE}.txt"
+    fi
+
+    echo $FILE
   else
-  
+
     echo ""
     echo "----------------------"
     echo "| To-Do List Manager |"
@@ -82,18 +94,18 @@ while true; do
     echo "| 1. List tasks      |"
     echo "| 2. Add task        |"
     echo "| 3. Remove task     |"
+    echo "| 4. Choose file     |"
     echo "| 0. Exit            |"
     echo "|--------------------|"
     echo ""
 
     echo $FILE_ICON $FILE
-    
 
     echo -n -e $ARROW_ICON " Choose an option [1-4]:\n"
     read option
-    
+
     case $option in
-    1 | "list" )
+    1 | "list")
       list_tasks
       echo ""
       ;;
@@ -102,6 +114,9 @@ while true; do
       ;;
     3)
       remove_task
+      ;;
+    4)
+      choose_file
       ;;
     0)
       echo "Goodbye!"
